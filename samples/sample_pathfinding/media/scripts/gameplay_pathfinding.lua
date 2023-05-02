62c6f6653e94706e97f1c266f82f7f511933d8c1
Benjamin Wrensch <benjamin.wrensch@icloud.com>
2023-05-02 15:37:38 +0200

    [add] pathfinding sample progress

--- samples/sample_pathfinding/media/scripts/gameplay_pathfinding.lua
+++ samples/sample_pathfinding/media/scripts/gameplay_pathfinding.lua
@@ -1,130 +1,50 @@
-World.load()
-Node.load()
-Math.load()
 Log.load()
-Random.load()
+DebugGeometry.load()
+Pathfinding.load()
+Entity.load()
+CharacterController.load()
 
-RoomSize = 2.9
-Extent = 30
+Maze = Utils.require("maze_generator")
 
-function SpawnWall(pos, dir)
-  local wall_n = World.spawn_prefab(string.format("wall_%s", dir)) 
-  Node.set_world_position(wall_n, Math.vec_add(pos, Vec3(0.0, 0.4, 0.0)))
-  Node.update_transforms(wall_n)
+ExecutePathfinding = false
 
-  return wall_n
-end
+function UpdatePathfinding()
+  local char = Entity.find_first_entity_with_name("char1")
+  local char_node = Node.get_component_for_entity(char)
+  local char_cct = CharacterController.get_component_for_entity(char)
+  local char_pos = Node.get_world_position(char_node)
 
-function RemoveWall(cell, dir)
-  Node.destroy(cell[dir])
-end
-
-function VecToDir(vec)
-  if vec.x == 0 and vec.y == 1 then
-    return "n"
-  elseif vec.x == 1 and vec.y == 0 then
-    return "e"
-  elseif vec.x == 0 and vec.y == -1 then
-    return "s"
+  if not TargetCell then
+    TargetCell = Maze.get_random_cell()
+    TargetPosition = Maze.get_center_position(TargetCell)
+    Path = Pathfinding.find_path(char_pos, TargetPosition)
   end
 
-  return "w"
-end
+  if Path then
+    Pathfinding.draw_debug_geometry(Path, false, true)
+    DebugGeometry.draw_sphere(TargetPosition, 1.0, Vec4(1.0, 0.0, 0.0, 1.0), false)
 
-function ConnectCells(from, to)
-  from_cell = Cells[from.y][from.x] 
-  to_cell = Cells[to.y][to.x] 
+    if not NextPosition then
+      local pos, success = Pathfinding.get_next_position_on_path(Path)
 
-  vec = {x=to.x-from.x,y=to.y-from.y}
-  vec_inv = {x=-vec.x, y=-vec.y}
-  dir = VecToDir(vec)
-  dir_inv = VecToDir(vec_inv)
-
-  RemoveWall(from_cell, dir)
-  RemoveWall(to_cell, dir_inv)
-end
-
-function GenerateWorld()
-  Cells = {}
-
-  local idx_x = 0
-  for x=-Extent/2, Extent/2 - 1 do
-    local cell_idx_x = Extent - 1 - idx_x
-
-    Cells[cell_idx_x + 1] = {}
-
-    local idx_y = 0
-    for y=-Extent/2, Extent/2 - 1 do
-      local cell_idx_y = Extent - 1 - idx_y
-
-      local n = SpawnWall(Vec3(x*RoomSize, 0.0, y*RoomSize), "n")
-      local e = SpawnWall(Vec3(x*RoomSize, 0.0, y*RoomSize), "e")
-      local s = SpawnWall(Vec3(x*RoomSize, 0.0, y*RoomSize), "s")
-      local w = SpawnWall(Vec3(x*RoomSize, 0.0, y*RoomSize), "w")
-
-      Cells[cell_idx_x + 1][cell_idx_y + 1] = { n=n, e=e, s=s, w=w }
-      idx_y = idx_y + 1
+      if success then
+        NextPosition = pos
+      end
     end
 
-    idx_x = idx_x + 1
-  end
-end
+    if NextPosition then
+      local to_target = Math.vec_sub(NextPosition, char_pos)
+      local dist = Math.vec_length(to_target)
 
-Maze = {}
+      if dist > 0 then
+        to_target = Math.vec_scale(1.0 / dist, to_target)
+      end
 
-function Maze.isCellValid(cell)
-  return cell.x >= 1 and cell.y >= 1 and cell.x <= Extent and cell.y <= Extent
-end
+      CharacterController.move(char_cct, Math.vec_scale(0.01, to_target))
 
-function Maze.addToFrontier(cell)
-  if not Maze.isCellValid(cell) then 
-    return
-  end
-
-  local gc = Maze.grid[cell.x][cell.y]
-  if not gc.frontier and not gc.inside then
-    gc.frontier = true
-    table.insert(Maze.frontier, cell)
-  end
-end
-
-function Maze.neighbors(cell)
-  local neighbors = {}
-  local coords = {
-    {x=cell.x + 1, y=cell.y},
-    {x=cell.x - 1, y=cell.y},
-    {x=cell.x, y=cell.y + 1},
-    {x=cell.x, y=cell.y - 1},
-  }
-
-  for i=1, #coords do
-    if Maze.isCellValid(coords[i]) and Maze.grid[coords[i].x][coords[i].y].inside then
-      table.insert(neighbors, coords[i])
-    end
-  end
-
-  return neighbors
-end
-
-function Maze.mark(cell)
-  Maze.grid[cell.x][cell.y].inside = true
-  Maze.addToFrontier({x=cell.x + 1, y=cell.y})
-  Maze.addToFrontier({x=cell.x - 1, y=cell.y})
-  Maze.addToFrontier({x=cell.x, y=cell.y + 1})
-  Maze.addToFrontier({x=cell.x, y=cell.y - 1})
-end
-
-function Maze.advance()
-  if #Maze.frontier > 0 then
-    local idx = Random.rand_uint_min_max(1, #Maze.frontier + 1)
-    cell = table.remove(Maze.frontier, idx)
-    neighbors = Maze.neighbors(cell)
-    if #neighbors > 0 then
-      local n_idx = Random.rand_uint_min_max(1, #neighbors + 1)
-      n = neighbors[n_idx]
-
-      ConnectCells(n, cell)
-      Maze.mark(cell)
+      if dist < 1.0 then
+        NextPosition, _ = Pathfinding.get_next_position_on_path(Path)
+      end
     end
   end
 end
@@ -132,17 +52,8 @@ end
 ---Called once when the script component becomes active.
 ---@param entity Ref The ref of the entity the script component is attached to.
 function OnActivate(entity)
-  GenerateWorld()
-
-  Maze.frontier = {}
-  Maze.grid = {}
-  for x=1, Extent do
-    Maze.grid[x] = {}
-    for y=1, Extent do
-      Maze.grid[x][y] = {inside=false, frontier=false}
-    end
-  end
-
+  Maze.generate_world()
+  Maze.initialize()
   Maze.mark({x=1, y=1})
 end
 
@@ -150,9 +61,19 @@ end
 ---@param entity Ref The ref of the entity the script component is attached to.
 ---@param delta_t number The time (in seconds) passed since the last call to this function.
 function Tick(entity, delta_t)
+  -- Increase this to speed up the maze generation
   local steps = 2
   for i=1,steps do
     Maze.advance()
+  end
+
+  -- Start executing pathfinding when we're done
+  if #Maze.frontier == 0 then
+    ExecutePathfinding = true
+  end
+
+  if ExecutePathfinding then
+    UpdatePathfinding()
   end
 end
 
